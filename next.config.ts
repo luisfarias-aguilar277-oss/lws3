c990b821863f0ca21bb5725445735f800ecfa749
luisfarias-aguilar277-oss <luis.farias-aguilar.277@my.csun.edu>
2025-11-10 21:36:26 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import { baseURL } from "./baseUrl";
 
 const nextConfig: NextConfig = {
-  assetPrefix: baseURL,
+  assetPrefix: baseURL,  // Prevents 404s on /_next/ files in iframe
 };
 
 export default nextConfig;
